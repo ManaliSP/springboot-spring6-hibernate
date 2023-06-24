c3e916f0ab4e89c29afdbacdadcdb2187155a594
ManaliSP <manalip054@gmail.com>
2023-06-24 08:22:24 +0530

    Use of Qualifiers

--- spring-module-02/src/main/java/com/springtutorial/springcoredemo/rest/DemoController.java
+++ spring-module-02/src/main/java/com/springtutorial/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.springtutorial.springcoredemo.rest;
 
 import com.springtutorial.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,21 +13,21 @@ public class DemoController {
     private Coach myCoach;
 
     //Constructor injection
-//    @Autowired
-//    public DemoController(Coach theCoach){
-//        myCoach = theCoach;
-//    }
-
-    //Setter Injection
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach){
         myCoach = theCoach;
     }
 
-    //Field Injection
-    @Autowired
-    private Coach coach;
-    
+    //Setter Injection
+//    @Autowired
+//    public void setCoach(Coach theCoach) {
+//        myCoach = theCoach;
+//    }
+//
+//    //Field Injection
+//    @Autowired
+//    private Coach coach;
+
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
