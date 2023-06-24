eb617ed891561f860357c13c807c2e66c2b537c7
ManaliSP <manalip054@gmail.com>
2023-06-24 11:42:05 +0530

    Java config beans - @Bean Code refining in controller

--- spring-module-02/src/main/java/com/springtutorial/springcoredemo/rest/DemoController.java
+++ spring-module-02/src/main/java/com/springtutorial/springcoredemo/rest/DemoController.java
@@ -11,7 +11,6 @@ public class DemoController {
 
     //define a private field for dependency
     private Coach myCoach;
-    private Coach anotherCoach;
 
     //Constructor injection
 //    @Autowired
@@ -19,31 +18,48 @@ public class DemoController {
 //        myCoach = theCoach;
 //    }
 
-      //Constructor injection using Qualifiers
-    @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
-                          @Qualifier("cricketCoach") Coach theAnotherCoach){
-        myCoach = theCoach;
-        anotherCoach = theAnotherCoach;
-    }
-
-      //Setter Injection
+    //Setter Injection
 //    @Autowired
 //    public void setCoach(Coach theCoach) {
 //        myCoach = theCoach;
 //    }
 
-      //Field Injection
+    //Field Injection
 //    @Autowired
 //    private Coach fieldCoach;
+
+
+    //Constructor injection using Qualifiers
+//    @Autowired
+//    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+//        System.out.println("In constructor: " + getClass().getSimpleName());
+//        myCoach = theCoach;
+//    }
+
+
+    //Check bean scopes
+//      private Coach anotherCoach;
+//    @Autowired
+//    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
+//                          @Qualifier("cricketCoach") Coach theAnotherCoach){
+//        myCoach = theCoach;
+//        anotherCoach = theAnotherCoach;
+//    }
+//      @GetMapping("/checkbeanscope")
+//    public String checkBeanSCope(){
+//        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
+//    }
+
+    //Bean injection using Qualifiers
+    //@Qualifier("aquatic") - using bean id
+    @Autowired
+    public DemoController(@Qualifier("swimCoach") Coach theCoach){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
-    }
-
-    @GetMapping("/checkbeanscope")
-    public String checkBeanSCope(){
-        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 }
