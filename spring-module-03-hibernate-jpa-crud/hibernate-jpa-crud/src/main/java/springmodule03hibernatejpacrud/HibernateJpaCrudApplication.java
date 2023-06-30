72b57a6a7db011dfb4b240be22616819e9b22d37
ManaliSP <manalip054@gmail.com>
2023-06-30 08:49:01 +0530

    Created new project added sql scripts

---
+++ spring-module-03-hibernate-jpa-crud/hibernate-jpa-crud/src/main/java/springmodule03hibernatejpacrud/HibernateJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package springmodule03hibernatejpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaCrudApplication.class, args);
+	}
+
+}
