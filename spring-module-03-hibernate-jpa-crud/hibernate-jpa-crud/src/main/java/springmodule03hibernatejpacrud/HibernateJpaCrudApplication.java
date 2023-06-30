1ca0c7aff236bd04bc24ef7f6777b7922c849af8
ManaliSP <manalip054@gmail.com>
2023-06-30 09:41:54 +0530

    added commandlinerunner added configurations for mysql and connected to DB

--- spring-module-03-hibernate-jpa-crud/hibernate-jpa-crud/src/main/java/springmodule03hibernatejpacrud/HibernateJpaCrudApplication.java
+++ spring-module-03-hibernate-jpa-crud/hibernate-jpa-crud/src/main/java/springmodule03hibernatejpacrud/HibernateJpaCrudApplication.java
@@ -1,13 +1,22 @@
 package springmodule03hibernatejpacrud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernateJpaCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateJpaCrudApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String args[]){
+		return runner -> {
+			System.out.println("Hello World..!!");
+		};
 	}
 
 }
