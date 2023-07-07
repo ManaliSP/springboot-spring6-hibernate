3b68b999110ca5a430aa316fb18c175e67ea4213
ManaliSP <manalip054@gmail.com>
2023-07-07 20:29:27 +0530

    implmemted api to add employee to DB and reflect on page using thymeleaf

--- spring-module-07-spring-Thymeleaf-CRUD/spring-boot-spring-mvc-thymeleaf-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ spring-module-07-spring-Thymeleaf-CRUD/spring-boot-spring-mvc-thymeleaf-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -33,7 +35,28 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
-		return "list-employees";
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model){
+
+		// create model attribute to bind form data
+		Employee employee = new Employee();
+
+		model.addAttribute("employee", employee);
+
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee){
+
+		// save the employee
+		employeeService.save(employee);
+
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
 	}
 }
 
