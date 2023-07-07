fd8c623fe3a6ce90c17228d57543537cd130f29d
ManaliSP <manalip054@gmail.com>
2023-07-07 23:55:31 +0530

    implemented update employee API using Thymeleaf

--- spring-module-07-spring-Thymeleaf-CRUD/spring-boot-spring-mvc-thymeleaf-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ spring-module-07-spring-Thymeleaf-CRUD/spring-boot-spring-mvc-thymeleaf-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,6 +46,19 @@ public class EmployeeController {
 		return "employees/employee-form";
 	}
 
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int id, Model model){
+
+		// get the employee from service
+		Employee employee = employeeService.findById(id);
+
+		// set employee in the model to prepopulate the form
+		model.addAttribute("employee", employee);
+
+		// send over to our form
+		return "employees/employee-form";
+	}
+
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee employee){
 
@@ -58,6 +68,8 @@ public class EmployeeController {
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/employees/list";
 	}
+
+
 }
 
 
