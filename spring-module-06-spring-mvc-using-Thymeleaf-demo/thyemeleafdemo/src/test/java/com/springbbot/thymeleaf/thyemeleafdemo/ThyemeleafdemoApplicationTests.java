c06a0a97d2fd33e905540c684510da6690e226ec
ManaliSP <manalip054@gmail.com>
2023-07-07 17:23:24 +0530

    project created for thymeleaf demo added dependency for thymeleaf

---
+++ spring-module-06-spring-mvc-using-Thymeleaf-demo/thyemeleafdemo/src/test/java/com/springbbot/thymeleaf/thyemeleafdemo/ThyemeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbbot.thymeleaf.thyemeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyemeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
