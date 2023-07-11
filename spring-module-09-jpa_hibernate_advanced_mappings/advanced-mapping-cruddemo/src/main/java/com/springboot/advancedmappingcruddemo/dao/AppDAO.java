c0c8508f4af4d3ffcd5176822ce9e7730b9e6bbf
ManaliSP <manalip054@gmail.com>
2023-07-11 14:12:13 +0530

    Implmented DAO layer

---
+++ spring-module-09-jpa_hibernate_advanced_mappings/advanced-mapping-cruddemo/src/main/java/com/springboot/advancedmappingcruddemo/dao/AppDAO.java
@@ -0,0 +1,7 @@
+package com.springboot.advancedmappingcruddemo.dao;
+
+import com.springboot.advancedmappingcruddemo.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+}
