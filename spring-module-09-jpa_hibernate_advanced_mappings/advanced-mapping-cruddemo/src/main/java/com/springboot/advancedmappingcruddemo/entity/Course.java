51afb37c5f1efa5c9123c180dafdb4638627413b
ManaliSP <manalip054@gmail.com>
2023-07-11 15:52:15 +0530

    Created entity class Course

---
+++ spring-module-09-jpa_hibernate_advanced_mappings/advanced-mapping-cruddemo/src/main/java/com/springboot/advancedmappingcruddemo/entity/Course.java
@@ -0,0 +1,66 @@
+package com.springboot.advancedmappingcruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    // annotate the class as an entity and map to db table
+
+    // define the fields and annotate the fields with db column name
+
+    // create constructor
+
+    // generate getter/setter method
+
+    // generate toString() method
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+    
+    public  Course(){
+
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
