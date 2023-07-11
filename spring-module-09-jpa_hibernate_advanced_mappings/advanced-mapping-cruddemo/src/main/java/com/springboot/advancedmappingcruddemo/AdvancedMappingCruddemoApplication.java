c56ce83afba06d2ecddc0c6ea68e011f491b3bf6
ManaliSP <manalip054@gmail.com>
2023-07-11 09:51:43 +0530

    project created

---
+++ spring-module-09-jpa_hibernate_advanced_mappings/advanced-mapping-cruddemo/src/main/java/com/springboot/advancedmappingcruddemo/AdvancedMappingCruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.advancedmappingcruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedMappingCruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedMappingCruddemoApplication.class, args);
+	}
+
+}
