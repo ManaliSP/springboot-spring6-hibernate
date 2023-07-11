d314a3de1af86d054b1f6dd1071b00de8c7306e3
ManaliSP <manalip054@gmail.com>
2023-07-11 19:04:07 +0530

    added @OneToMany relationship type added fetch type, cascade type and @JoinColumn name

--- spring-module-09-jpa_hibernate_advanced_mappings/advanced-mapping-cruddemo/src/main/java/com/springboot/advancedmappingcruddemo/entity/Course.java
+++ spring-module-09-jpa_hibernate_advanced_mappings/advanced-mapping-cruddemo/src/main/java/com/springboot/advancedmappingcruddemo/entity/Course.java
@@ -30,7 +30,9 @@ public class Course {
     @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
-    
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
     public  Course(){
