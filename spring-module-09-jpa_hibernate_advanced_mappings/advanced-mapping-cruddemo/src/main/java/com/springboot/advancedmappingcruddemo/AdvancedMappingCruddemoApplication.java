86c0c4bf2d774c72c38a96d27528a86312d26ba3
ManaliSP <manalip054@gmail.com>
2023-07-11 10:27:32 +0530

    Creted commandLineRunner Added JDBC configuration

--- spring-module-09-jpa_hibernate_advanced_mappings/advanced-mapping-cruddemo/src/main/java/com/springboot/advancedmappingcruddemo/AdvancedMappingCruddemoApplication.java
+++ spring-module-09-jpa_hibernate_advanced_mappings/advanced-mapping-cruddemo/src/main/java/com/springboot/advancedmappingcruddemo/AdvancedMappingCruddemoApplication.java
@@ -1,13 +1,22 @@
 package com.springboot.advancedmappingcruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdvancedMappingCruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedMappingCruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String args[]){
+		return runner -> {
+			System.out.println("Hello World!");
+		};
 	}
 
 }
