058e1d5d64a23ae9447108a78b10e2b437487041
ManaliSP <manalip054@gmail.com>
2023-07-11 20:16:40 +0530

    Created Entity class Student

---
+++ spring-module-09-jpa_hibernate_advanced_mappings/advanced-mapping-cruddemo/src/main/java/com/springboot/advancedmappingcruddemo/entity/Student.java
@@ -0,0 +1,86 @@
+package com.springboot.advancedmappingcruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    // annotate the class as an entity and map to db table
+
+    // define the fields and annotate the fields with db column name
+
+    // ** set up mapping to Course entity
+
+    // create constructor
+
+    // generate getter/setter method
+
+    // generate toString() method
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public Student(){
+
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
