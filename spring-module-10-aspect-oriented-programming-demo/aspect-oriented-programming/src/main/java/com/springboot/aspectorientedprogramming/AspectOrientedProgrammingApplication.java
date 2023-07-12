23ecc5d85bb247de0f72a51bed623a0316c755cc
ManaliSP <manalip054@gmail.com>
2023-07-12 15:12:43 +0530

    added command line runner

--- spring-module-10-aspect-oriented-programming-demo/aspect-oriented-programming/src/main/java/com/springboot/aspectorientedprogramming/AspectOrientedProgrammingApplication.java
+++ spring-module-10-aspect-oriented-programming-demo/aspect-oriented-programming/src/main/java/com/springboot/aspectorientedprogramming/AspectOrientedProgrammingApplication.java
@@ -1,7 +1,9 @@
 package com.springboot.aspectorientedprogramming;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AspectOrientedProgrammingApplication {
@@ -10,4 +12,10 @@ public class AspectOrientedProgrammingApplication {
 		SpringApplication.run(AspectOrientedProgrammingApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String args[]){
+		return runner -> {
+			System.out.println("Hello World!");
+		};
+	}
 }
