0c6c6d784483a267034d2499fc59424018873006
ManaliSP <manalip054@gmail.com>
2023-07-12 17:34:04 +0530

    Added pointcut declaration

--- spring-module-10-aspect-oriented-programming-demo/aspect-oriented-programming/src/main/java/com/springboot/aspectorientedprogramming/aspect/MyLoggingAspect.java
+++ spring-module-10-aspect-oriented-programming-demo/aspect-oriented-programming/src/main/java/com/springboot/aspectorientedprogramming/aspect/MyLoggingAspect.java
@@ -2,6 +2,7 @@ package com.springboot.aspectorientedprogramming.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,34 +13,45 @@ public class MyLoggingAspect {
 
     // let's start with @Before aspect
 
-    // pointcut expression - match on method name in any class
-//    @Before("execution(public void addAccount())")
+/*
+Pointcut expressions exmaples for reference
 
-    // pointcut expression - match on method name in specific class/interface
-//    @Before("execution(public void  com.springboot.aspectorientedprogramming.dao.AccountDAO.addAccount())")
+    pointcut expression - match on method name in any class
+    @Before("execution(public void addAccount())")
 
-    // pointcut expression - match on method name any add*
-//    @Before("execution(public void add*())")
+     pointcut expression - match on method name in specific class/interface
+    @Before("execution(public void  com.springboot.aspectorientedprogramming.dao.AccountDAO.addAccount())")
 
-    // pointcut expression - match method with bases on return type
-//    @Before("execution(void add*())")
+     pointcut expression - match on method name any add*
+    @Before("execution(public void add*())")
 
-    // pointcut expression - match method with any return type
-//    @Before("execution(* add*())")
+     pointcut expression - match method with bases on return type
+    @Before("execution(void add*())")
 
-    // pointcut expression - match method on method parameters
+     pointcut expression - match method with any return type
+    @Before("execution(* add*())")
 
-    // match method on Account parameters
-//    @Before("execution(* add*(com.springboot.aspectorientedprogramming.Account))")
+     pointcut expression - match method on method parameters
 
-    // match method on more parameters
-//    @Before("execution(* add*(com.springboot.aspectorientedprogramming.Account, ..))")
+     match method on Account parameters
+    @Before("execution(* add*(com.springboot.aspectorientedprogramming.Account))")
 
-    // pointcut expression - match method on any parameters
-//    @Before("execution(* add*(..))")
+     match method on more parameters
+    @Before("execution(* add*(com.springboot.aspectorientedprogramming.Account, ..))")
 
-    // pointcut expression - match method in a package
+     pointcut expression - match method on any parameters
+    @Before("execution(* add*(..))")
+
+     pointcut expression - match method in a package
     @Before("execution(* com.springboot.aspectorientedprogramming.dao.*.*(..))")
+*/
+
+  // Pointcut declaration
+    @Pointcut("execution(* com.springboot.aspectorientedprogramming.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    // Use pointcut declaration
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice(){
 
         System.out.println("\n=====> Executing @Before advice on addAccount()");
