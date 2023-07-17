bc8232572a83380adea39fb5b6d1a5795e5bbf78
ManaliSP <manalip054@gmail.com>
2023-07-17 14:07:32 +0530

    setup pointcut declarations

--- spring-module-10-1-aop-springmvc-crud-app/spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ spring-module-10-1-aop-springmvc-crud-app/spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -11,5 +12,15 @@ public class DemoLoggingAspect {
 
     // setup logger
     private Logger logger = Logger.getLogger(getClass().getName());
-    
+
+    // setup pointcut declarations
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..))")
+    private void forControllerPackage(){}
+
+    // do same for service and dao
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..))")
+    private void forServicePackage(){}
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")
+    private void forDaoPackage(){}
 }
