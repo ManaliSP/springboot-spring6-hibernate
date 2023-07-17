14ad44f849d1a8b84027699adfb8ac8075d729c8
ManaliSP <manalip054@gmail.com>
2023-07-17 15:58:52 +0530

    Project created

---
+++ spring-module-11-spring-form-validation/hibernate-form-validator/src/main/java/com/springboot/hibernateformvalidator/HibernateFormValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hibernateformvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateFormValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateFormValidatorApplication.class, args);
+	}
+
+}
---
+++ spring-module-11-spring-form-validation/hibernate-form-validator/src/test/java/com/springboot/hibernateformvalidator/HibernateFormValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hibernateformvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateFormValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
